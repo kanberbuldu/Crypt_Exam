2267ee379a4742c75ca02214e82c0dc8df766878
Shin <kanberbuldu@gmail.com>
2024-01-06 15:54:34 +0300

    All file Updated

--- Catch The Turtle/Catch.py
+++ Catch The Turtle/Catch.py
@@ -1,21 +1,24 @@
 import time
 import turtle
 
-time_line = 10
+time_line = 15
 while (time_line) > 0:
 
-    time.sleep(1)
+    time.sleep(0.5)
     print(time_line)
 
     disp = turtle.Screen()
+
     turtle_ins = turtle.Turtle()
+
     turtle_ins.hideturtle()
+    turtle_ins.pencolor("blue")
     turtle_ins.write(time_line, font=("Arial", 14, "normal"))
     time.sleep(0.5)
-    turtle_ins.clear()
+
 
     time_line -= 1
-
+    turtle_ins.clear()
     if time_line == 0:
         turtle_ins.write("Game Over", font=("Arial", 14, "normal"))
 
